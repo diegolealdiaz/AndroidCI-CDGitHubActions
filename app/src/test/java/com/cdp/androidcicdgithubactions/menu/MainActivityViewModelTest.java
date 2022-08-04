d04109088bb55d2638619b0d8b67c2cd3f1f42f3
Diego Leal <diegoleal2293@gmail.com>
2022-08-04 09:09:01 -0500

    Prueba 4 Pipeline Unit Test

--- app/src/test/java/com/cdp/androidcicdgithubactions/menu/MainActivityViewModelTest.java
+++ app/src/test/java/com/cdp/androidcicdgithubactions/menu/MainActivityViewModelTest.java
@@ -34,7 +34,7 @@ public class MainActivityViewModelTest {
 
     @Test
     public void resta() throws Exception {
-        assertEquals(-1,mainActivityViewModel.operationData(5,6,"Restar"));
+        assertEquals(-2,mainActivityViewModel.operationData(5,6,"Restar"));
     }
 
     @Test
